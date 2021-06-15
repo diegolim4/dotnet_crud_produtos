4e11069237606feefd29ac5c1671e94b1e6055b5
Diego <lima.j.diego@gmail.com>
2021-06-15 10:26:09 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD.Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
