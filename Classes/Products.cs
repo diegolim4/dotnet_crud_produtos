7c8f5e4d5cc20d60fc9b02939ae01b8119cf7694
Diego <lima.j.diego@gmail.com>
2021-06-15 13:00:29 -0300

    implementação do to string

--- Classes/Products.cs
+++ Classes/Products.cs
@@ -1,11 +1,13 @@
+using System;
+
 namespace CRUD.Produtos
 {
     public class Products : EntidadeBase // Essa classe vai herda da class base
     {
         //Atributos
-        private  string Name {get; set;}
-        private string Description {get; set;}
-        private int Price{ get; set;}
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private int Price { get; set; }
 
         // Métodos
         public Products(int id, string name, string description, int price)
@@ -13,7 +15,17 @@ namespace CRUD.Produtos
             this.Id = id;
             this.Name = name;
             this.Description = description;
-            this.Price = price; 
+            this.Price = price;
+        }
+
+        // Implementação do to string
+        public override string ToString()
+        {
+            string show = "";
+            show += "Nome: " + this.Name + Environment.NewLine;
+            show += "Descrição: " + this.Description + Environment.NewLine;
+            show += "Preço: " + this.Price + Environment.NewLine;
+            return show;
         }
     }
 }
